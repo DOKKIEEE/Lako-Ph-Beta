02125a2e81049cfcdf67ff824a9ba204392cf2ad
Dokkie <wyvern@dokkie.net>
2022-12-06 09:35:39 +0800

    Lako Ph v2.5

--- js/index.js
+++ js/index.js
@@ -1,68 +1,42 @@
-// log in
-const loginDisplay = document.querySelector("#loginDisplay");
-const login = document.querySelector("#login");
-login.addEventListener("click", userLogin);
-function userLogin () {
-    loginDisplay.style.display = "flex";
-}
-// close login button
-const closeBtn = document.querySelector("#closeButton")
-closeBtn.addEventListener("click", xBtn);
-function xBtn() {
-    loginDisplay.style.display = "none";
-}
-// login validation function
-const userName = document.querySelector("#username");
-const password = document.querySelector("#password");
-const userError = document.querySelector("#userError");
-const passError = document.querySelector("#passwordError");
-const submitBtn = document.querySelector("#submit");
-submitBtn.addEventListener("click", submitButton);
+// login validation
+let loginBtn = document.querySelector("#loginSubmit");
+loginBtn.addEventListener("click", loginSecured);
 
- function submitButton () {
-    
-     if (userName.value === ""){
-         userError.style.display = "block";
-         userError.innerHTML = "Username required";
-         userError.style.color = "red";
-         userName.style.boxShadow = "1px -2px 2px 2px red, -2px 2px 2px 2px red ";
-     }
-     else if (password.value.length < 4) {
-        passError.style.display = "block";
-        passError.innerHTML = "Password must be at least 4 characters";
-        passError.style.color = "red";
-        password.style.boxShadow = "1px -2px 2px 2px red, -2px 2px 2px 2px red ";
-     }  
-     else if (password.value.length > 10) {
-        passError.style.display = "block";
-        passError.innerHTML = "Password must not exceed 10 characters";
-        passError.style.color = "red";
-        password.style.boxShadow = "1px -2px 2px 2px red, -2px 2px 2px 2px red ";
-     }
-     else if (userName.value != "user") {
-        userError.style.display = "block";
-        userError.innerHTML = "Check Username";
-        userError.style.color = "red";
-        userName.style.boxShadow = "1px -2px 2px 2px red, -2px 2px 2px 2px red ";
-     }
-     else if (password.value != 1234) {
-        passError.style.display = "block";
-        passError.innerHTML = "Check Password";
-        passError.style.color = "red";
-        password.style.boxShadow = "1px -2px 2px 2px red, -2px 2px 2px 2px red ";
-     }
-     else if (userName.value != "user" && password.value != 1234) {
-        userError.style.display = "block";
-        userError.innerHTML = "Check Username";
-        userError.style.color = "red";
-        userName.style.boxShadow = "1px -2px 2px 2px red, -2px 2px 2px 2px red ";
-        passError.style.display = "block";
-        passError.innerHTML = "Check Password";
-        passError.style.color = "red";
-        password.style.boxShadow = "1px -2px 2px 2px red, -2px 2px 2px 2px red ";
-     }
-     else if(userName.value == "user" && password.value == 1234){
-        window.location.href = "https://dokkieee.github.io/LakoPh/userInterface.html";
-     }
-     
- }
+const registeredUsername = "user";
+const registeredUserPass = "1234";
+
+const errorUsernameDisplay = document.querySelector("#usernameError");
+const errorPasswordDisplay = document.querySelector("#passwordError");
+
+const usernameErrorMsg = document.createElement("p");
+usernameErrorMsg.textContent= "Invalid Username";
+usernameErrorMsg.classList = "usernameError";
+usernameErrorMsg.style.color = "red"; 
+
+const passwordErrorMsg = document.createElement("p");
+passwordErrorMsg.textContent= "Invalid Password";
+passwordErrorMsg.classList = "passwordError";
+passwordErrorMsg.style.color = "red";  
+
+
+   function loginSecured() {
+      const userNameInput = document.querySelector("#username").value;
+      console.log(userNameInput);
+
+      const userPasswordInput = document.querySelector("#password").value;
+      console.log(userPasswordInput);
+
+      if(userNameInput != registeredUsername && userPasswordInput != registeredUserPass) {
+         errorUsernameDisplay.appendChild(usernameErrorMsg);
+         errorPasswordDisplay.appendChild(passwordErrorMsg);
+      } else if (userNameInput != registeredUsername && userPasswordInput === registeredUserPass){
+         errorUsernameDisplay.appendChild(usernameErrorMsg);
+         errorPasswordDisplay.removeChild(passwordErrorMsg);
+      } else if (userNameInput === registeredUsername && userPasswordInput != registeredUserPass){
+         errorUsernameDisplay.removeChild(usernameErrorMsg);
+         errorPasswordDisplay.appendChild(passwordErrorMsg);
+      } else if (userNameInput === registeredUsername && userPasswordInput === registeredUserPass){
+         setTimeout(function(){ window.location.href = "https://dokkieee.github.io/LakoPh/userInterface.html"; }, 100);
+         console.log("login successfull");
+      }
+   }
